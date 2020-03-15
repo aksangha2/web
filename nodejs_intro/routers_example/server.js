624a28631b8c0704bc6b35241645841c9a530c71
aksangha <aksangha2@gmail.com>
2020-03-16 00:10:18 +0530

    routers intro

--- nodejs_intro/routers_example/server.js
+++ nodejs_intro/routers_example/server.js
@@ -4,6 +4,10 @@ const server=express()
 const teacherRoute=require('./routes/teachers')
 const studentRoute=require('./routes/students')
 
+
+server.use(express.json())     /*json middleware-can decode json body to js*/
+server.use(express.urlencoded({extended:true}))  /*if body in url encoded format*/
+
 server.use('/students',studentRoute)
 server.use('/teachers',teacherRoute)
 
